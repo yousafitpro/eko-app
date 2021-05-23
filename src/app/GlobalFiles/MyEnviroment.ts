d2c6d812fca6389ccab0124a103c0bbf7e238a06
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2021-05-23 10:57:37 -0700

    ok

--- src/app/GlobalFiles/MyEnviroment.ts
+++ src/app/GlobalFiles/MyEnviroment.ts
@@ -8,15 +8,15 @@ export class MyEnviroment{
     public static AppID:any="001";
 
 
-    public static afterLoginUrl="http://localhost/productProd/mobile/mobile/"
-    public static afterLogoutUrl="http://localhost/productProd/mobile/mobile/"
-    public static refreshURL="http://localhost/productProd/mobile/"
+    public static afterLoginUrl="http://localhost/product/eko-app/"
+    public static afterLogoutUrl="http://localhost/product/eko-app/"
+    public static refreshURL="http://localhost/product/eko-app/"
     // public static coreDomain:any="http://127.0.0.1:8000";
     // public static baseUrl:any=MyEnviroment.coreDomain+"/api/"; 
     // public static coreISS:any=MyEnviroment.coreDomain+"/api/auth/login";
     
     // http://core1.ekob.online/public/api/
-    public static coreDomain:any="http://localhost/productProd/backend";
+    public static coreDomain:any="http://localhost/product/eko-api";
     public static baseUrl:any=MyEnviroment.coreDomain+"/public/api/";
     public static coreISS:any=MyEnviroment.coreDomain+"/public/api/auth/login";
     
